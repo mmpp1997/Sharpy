4b662b3c1ed1c4ded6ef703d6f3d2d2be05e0608
mmpp1997 <108473365+mmpp1997@users.noreply.github.com>
2022-08-07 11:36:40 +0200

    zavrsni commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Sharpy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(12, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Location = new System.Drawing.Point(13, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(284, 19);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Say Wake Up to activate Sharpy";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox2.Location = new System.Drawing.Point(13, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(284, 19);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(93, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Sharpy is not activated";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(13, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "You:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(13, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Sharpy:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Sharpy.Properties.Resources.AI_world_forum_background_cropped_1920x1000;
+            this.ClientSize = new System.Drawing.Size(313, 144);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace Sharpy
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine Spec = new SpeechRecognitionEngine();
+
+        SpeechSynthesizer Sharpy = new SpeechSynthesizer();
+
+        DateTime danas = DateTime.UtcNow.Date;
+        public Form1()
+        {
+            Spec.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Start);
+            Naredbe();
+            Spec.SetInputToDefaultAudioDevice();
+            Spec.RecognizeAsync(RecognizeMode.Multiple);
+            InitializeComponent();
+        }
+      
+        private void Naredbe()
+        {
+            Choices mogucnosti = new Choices();
+            string[] naredbe = File.ReadAllLines(Environment.CurrentDirectory + "\\naredbe.txt");
+            mogucnosti.Add(naredbe);
+            Grammar rijecnik = new Grammar(new GrammarBuilder(mogucnosti));
+            Spec.LoadGrammar(rijecnik);
+        }
+        private void Start(object sender, SpeechRecognizedEventArgs e)
+        {
+            string govor = e.Result.Text;          
+            if (govor == "wake up" )
+            {             
+                Spec.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Spec_SpeechRecognized);
+                Spec.SpeechRecognized -= new EventHandler<SpeechRecognizedEventArgs>(Start);
+
+                Sharpy.Speak("how can i assist you today");
+                textBox1.Text = "How can i Assist you today?";
+                textBox2.Text = "Hint: Ask Sharpy: What can you do?";
+                label1.Text = "Sharpy is now active";
+                label1.BackColor = System.Drawing.Color.Green;
+            }
+        }
+        private void Spec_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string govor = e.Result.Text;
+            switch (govor)
+            {
+                case "whats up":
+                    Sharpy.Speak("not much");
+                    textBox1.Text = "What's up?";
+                    textBox2.Text = "Not Much.";
+                    break;
+
+                case "can you open notepad":
+                    Sharpy.Speak("sure just a second");
+                    textBox1.Text = "Can you open notepad?";
+                    textBox2.Text = "Sure, just a second.";
+                    Process.Start("notepad.exe");
+                    break;
+
+                case "can you close notepad":
+                    Sharpy.Speak("sure just a second");
+                    textBox1.Text = "Can you close notepad?";
+                    textBox2.Text = "Sure, just a second.";
+                    Izvrsi("c:/nircmd.exe killprocess notepad.exe");
+                    break;
+
+                case "hide":
+                    Sharpy.Speak("will do");
+                    textBox1.Text = "Hide!";
+                    textBox2.Text = "Will do!";
+                    this.WindowState = FormWindowState.Minimized;
+                    break;
+
+                case "can you tell me the date":
+                    Sharpy.Speak("sure today is " + danas.Date.ToString());
+                    textBox1.Text = "Can you tell me the date?";
+                    textBox2.Text = "Sure, Today is " + danas.Date.ToString();
+                    break;
+
+                case "how are you sharpy":
+                    Sharpy.Speak("i am fine How are you");
+                    textBox1.Text = "How are you Sharpy?";
+                    textBox2.Text = "I'm fine. How are you?";
+                    break;
+
+                case "i am ok":
+                    Sharpy.Speak("that is nice");
+                    textBox1.Text = "Im OK!";
+                    textBox2.Text = "That is nice";
+                    break;
+
+                
+
+                
+                    
+                
+
+                case "can you go to the web":
+                    Sharpy.Speak("sure just a second");
+                    textBox1.Text = "Can you go to the web?";
+                    textBox2.Text = "Sure, just a second.";
+                    Process.Start("MicrosoftEdge.exe");
+                    break;
+
+                case "can you close edge":
+                    Sharpy.Speak("sure just a second");
+                    textBox1.Text = "Can you close edge?";
+                    textBox2.Text = "Sure, just a second.";
+                    Izvrsi("c:/nircmd.exe killprocess MicrosoftEdge.exe");
+                    break;
+
+                case "change sound":
+                    Sharpy.Speak("sure just a second");
+                    textBox1.Text = "Change sound!";
+                    textBox2.Text = "Sure, just a second.";
+                    Izvrsi("C:/nircmd.exe mutesysvolume 2 ");
+                    break;
+
+                case "what did i copy":
+                    Sharpy.Speak("You copied this");
+                    textBox1.Text = "What did i copy?";
+                    textBox2.Text = Clipboard.GetText(TextDataFormat.Text);
+                    break;
+
+                case "what can you do":
+                    Sharpy.Speak("let me show you");
+                    textBox1.Text = "What can you do?";
+                    textBox2.Text = "Let me show you!";
+                    Process.Start("notepad.exe", Environment.CurrentDirectory + "\\mogucnosti.txt");
+                    break;
+
+                case "that is all":
+                    Sharpy.Speak("bye hear from you soon");
+                    textBox1.Text = "That is all";
+                    textBox2.Text = "Bye, hear from you soon!";
+                    Application.Exit();
+                    break;
+
+                case "can you check the weather":
+                    Sharpy.Speak("checking now");
+                    textBox1.Text = "Can you check the weather?";
+                    textBox2.Text = "Checking now!";
+                    Process.Start("chrome.exe", "https://meteo.hr/prognoze.php?Code=Split&id=prognoza&section=prognoze_model&param=3d");
+                    break;
+
+                case "please open task manager":
+                    Sharpy.Speak("i will");
+                    textBox1.Text = "Please open task manager!";
+                    textBox2.Text = "I will!";
+                    Process.Start("Taskmgr.exe");
+                    break;
+
+                case "open quick access":
+                    Sharpy.Speak("here you go");
+                    textBox1.Text = "Open Quick access!";
+                    textBox2.Text = "Here you go!";
+                    Process.Start(@"C:\Users\Mario");
+                    break;
+
+                case "start":
+                    Sharpy.Speak("on screen right now");
+                    textBox1.Text = "Start!";
+                    textBox2.Text = "On screen right now!";
+                    SendKeys.Send("^{ESC}");                  
+                    break;
+     
+                
+
+                case "maximize":
+                    Sharpy.Speak("will do");
+                    textBox1.Text = "Maksimize!";
+                    textBox2.Text = "Will do!";
+                    this.WindowState = FormWindowState.Maximized;
+                    break;
+
+                case "open cmd":
+                    Sharpy.Speak("opening cmd");
+                    textBox1.Text = "open CMD!";
+                    textBox2.Text = "Opening CMD";
+                    Process.Start("cmd.exe");
+                    break;
+            }
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        public static void Izvrsi(string naredba)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd", "/c" + naredba);
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.CreateNoWindow = true;
+            Process pro = new Process();
+            pro.StartInfo = processStartInfo;
+
+            pro.Start();
+            pro.Close();
+        }
+    }
+}
